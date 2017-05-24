ed2084c6f493e4fac11d26922d6e65790a6a9045
Rafic Jouejati <Rafic Jouejati>
2017-05-24 19:07:22 -0400

    geolocation api

--- public/js/google-api.js
+++ public/js/google-api.js
@@ -1,20 +1,13 @@
- navigator.geolocation.getCurrentPosition(success, error);
+navigator.geolocation.getCurrentPosition(success);
 
         function success(position) {
 
             var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
 
             $.getJSON(GEOCODING).done(function(location) {
-                //$('#country').html(location.results[0].address_components[5].long_name);
-                $('#state').html(location.results[0].address_components[4].long_name);
-                $('#city').html(location.results[0].address_components[2].long_name);
-                //$('#address').html(location.results[0].formatted_address);
-                //$('#latitude').html(position.coords.latitude);
-                //$('#longitude').html(position.coords.longitude);
+                $('#state').val(location.results[0].address_components[4].long_name);
+                $('#city').val(location.results[0].address_components[2].long_name);
+
             })
 
-        }
-
-        function error(err) {
-            console.log(err)
         }
